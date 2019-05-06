7961254a6b890d9c66d14bee5d5a960a7d5df375
Ravinder Mahajan <ravindermahajan890@gmail.com>
2019-05-06 11:45:22 +0530

    Updating editor config files

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./demo.scss";
 const Demo = props => {
--- src/components/Routes/Route.js
+++ src/components/Routes/Route.js
@@ -3,26 +3,24 @@ import { Route, BrowserRouter, Link, Switch } from "react-router-dom";
 import Example from "../Example/Example";
 // import Gallery from "./test";
 import "../../styles/index.scss";
-const App = () => {
-  return (
-    <BrowserRouter>
+const App = () => (
+  <BrowserRouter>
+    <div>
       <div>
-        <div>
-          <div className="nav">
-            <Link to="/Gallery">Gallery</Link>
-            <Link to="/home">home</Link>
-            <Link to="/Example">Example with react hooks</Link>
-          </div>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route path="/home" component={Demo} />
-              <Route path="/example" component={Example} />
-            </Switch>
-          </Suspense>
+        <div className="nav">
+          <Link to="/Gallery">Gallery</Link>
+          <Link to="/home">home</Link>
+          <Link to="/Example">Example with react hooks</Link>
         </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/home" component={Demo} />
+            <Route path="/example" component={Example} />
+          </Switch>
+        </Suspense>
       </div>
-    </BrowserRouter>
-  );
-};
+    </div>
+  </BrowserRouter>
+);
 const Demo = lazy(() => import(/* webpackChunkName:"home" */ "../Demo/Demo"));
 export default App;
