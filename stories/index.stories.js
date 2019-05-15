d5861daf5323594b74898d518798f04347ad6526
Ravinder Mahajan <ravindermahajan890@gmail.com>
2019-05-15 09:48:00 +0530

    Correcting import location in storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,5 +3,5 @@ import React from "react";
 import { storiesOf, addDecorator } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 addDecorator(withInfo({ source: true }));
-import Home from "../src/components/Demo/Demo";
+import Home from "../src/components/Demo";
 storiesOf("Demo", module).add("demo component", () => <Home name="Jane Doe" />);
